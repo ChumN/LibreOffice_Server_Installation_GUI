f2848657f96d1e945d92b8093413ccaff20e3871
Florian Reisinger <reisi007@gmail.com>
2012-12-01 17:09:55 +0100

    Updated languages

--- Manager.Designer.cs
+++ Manager.Designer.cs
@@ -43,7 +43,7 @@
             this.manager_list.FormattingEnabled = true;
             this.manager_list.Location = new System.Drawing.Point(12, 12);
             this.manager_list.Name = "manager_list";
-            this.manager_list.Size = new System.Drawing.Size(316, 214);
+            this.manager_list.Size = new System.Drawing.Size(560, 214);
             this.manager_list.TabIndex = 0;
             // 
             // button1
@@ -52,7 +52,7 @@
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Location = new System.Drawing.Point(12, 227);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(57, 23);
+            this.button1.Size = new System.Drawing.Size(92, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Exit";
             this.button1.UseVisualStyleBackColor = true;
@@ -63,9 +63,9 @@
             this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Location = new System.Drawing.Point(217, 227);
+            this.button2.Location = new System.Drawing.Point(378, 227);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.Size = new System.Drawing.Size(194, 23);
             this.button2.TabIndex = 2;
             this.button2.Text = "Delete selected";
             this.button2.UseVisualStyleBackColor = true;
@@ -74,9 +74,9 @@
             // button3
             // 
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Location = new System.Drawing.Point(75, 227);
+            this.button3.Location = new System.Drawing.Point(110, 227);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(136, 23);
+            this.button3.Size = new System.Drawing.Size(262, 23);
             this.button3.TabIndex = 3;
             this.button3.Text = "Manually add installation";
             this.button3.UseVisualStyleBackColor = true;
@@ -86,13 +86,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(340, 262);
+            this.ClientSize = new System.Drawing.Size(584, 262);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.manager_list);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.MinimumSize = new System.Drawing.Size(356, 300);
+            this.MaximumSize = new System.Drawing.Size(600, 600);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
             this.Name = "Manager";
             this.Text = "Manager";
             this.Load += new System.EventHandler(this.Manager_Load);
